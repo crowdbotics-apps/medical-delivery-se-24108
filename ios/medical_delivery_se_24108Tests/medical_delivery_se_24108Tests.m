2ed7b90538662fc88ecdb0053b4fbe7e694bff2d
Crowdbotics <team@crowdbotics.com>
2021-01-27 21:21:05 +0000

    Initial commit.

---
+++ ios/medical_delivery_se_24108Tests/medical_delivery_se_24108Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface medical_delivery_se_24108 : XCTestCase
+
+@end
+
+@implementation medical_delivery_se_24108
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end

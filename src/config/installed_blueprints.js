a08b385af94062b86834f0265e1165a1712e9e93
Crowdbotics <team@crowdbotics.com>
2021-01-27 21:21:22 +0000

    add package blueprint-react-native-sign-up-2-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignUp22197363', human_name: 'Sign Up 2 2', access_route: 'SignUp22197363'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
